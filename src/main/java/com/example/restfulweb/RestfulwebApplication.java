1295cc6a68543365e9f25a5606237fbc81c04a58
Lane Anderson <greatlakes909@gmail.com>
2024-09-05 17:37:53 -0700

    Hello this is my first Spring Boot project

---
+++ src/main/java/com/example/restfulweb/RestfulwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulwebApplication.class, args);
+	}
+
+}
